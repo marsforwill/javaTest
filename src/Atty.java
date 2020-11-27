9305f35d122698cda851f6e3b24d60bc55aeedee
liushimin <liushimin@sensetime.com>
2020-11-27 11:13:39 +0800

    init

---
+++ src/Atty.java
@@ -0,0 +1,6 @@
+public class Atty {
+
+    public static void main(String[] args) {
+        System.out.println("atty");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+//        //HMAC
+//        Algorithm algorithmHS = Algorithm.HMAC256("Ac0vkWwbSgBuOqJKGUw8uKlCWv7v0FNP");
+//
+//        //RSA
+//        RSAPublicKey publicKey = null;//Get the key instance
+//        RSAPrivateKey privateKey = null;//Get the key instance
+//        Algorithm algorithmRS = Algorithm.RSA256(publicKey, privateKey);
+    }
+}
