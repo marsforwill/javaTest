0a3bbed4d83c4887486596131bf16b558fdf90f3
liushimin <liushimin@sensetime.com>
2020-11-29 18:02:13 +0800

    pour water

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import com.auth0.jwt.algorithms.Algorithm;
-
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.util.Stack;
 
 public class Main {
@@ -26,15 +22,63 @@ public class Main {
         return f;
     }
 
+    // 理解题意的意思是： 是包括大于等于来落的，但是最后落下的一定是 尽量低 ，再次是尽量靠近落点 的题意 就很奇葩
+    public static int[] pourWater(int[] heights, int V, int K) {
+        for (int i = 0; i < V; i++) {
+            if (K-1 >= 0 && heights[K-1] <= heights[K]) {
+                int temp = K,j=K;
+                while (j-1 >= 0 && heights[j-1] <= heights[j]) {
+                    if (heights[j-1] < heights[j]) {
+                        temp=j-1;
+                    }
+                    j--;
+                }
+                if (heights[temp] < heights[K]) {
+                    heights[temp]++;
+                    continue;
+                }
+            }
+            if (K+1 < heights.length && heights[K+1] <= heights[K]) {
+                int temp = K, j=K;
+                while (j+1 < heights.length && heights[j+1] <= heights[j]) {
+                    if (heights[j+1] < heights[j]) {
+                        temp=j+1;
+                    }
+                    j++;
+                }
+                if (heights[temp] < heights[K]) {
+                    heights[temp]++;
+                    continue;
+                }
+            }
+            heights[K]++;
+
+        }
+        return heights;
+    }
+
+    public static int[] pourWater2(int[] H, int V, int K) {
+        while (V-- > 0) droplet: {
+            for (int d = -1; d <= 1; d += 2) {
+                int i = K, best = K;
+                while (0 <= i+d && i+d < H.length && H[i+d] <= H[i]) {
+                    if (H[i+d] < H[i]) best = i + d;
+                    i += d;
+                }
+                if (H[best] < H[K]) {
+                    H[best]++;
+                    break droplet;
+                }
+            }
+            H[K]++;
+        }
+        return H;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println(mostCompetitive(new int[]{71,18,52,29,55,73,24,42,66,8,80,2}, 3));
-//        //HMAC
-//        Algorithm algorithmHS = Algorithm.HMAC256("Ac0vkWwbSgBuOqJKGUw8uKlCWv7v0FNP");
-//
-//        //RSA
-//        RSAPublicKey publicKey = null;//Get the key instance
-//        RSAPrivateKey privateKey = null;//Get the key instance
-//        Algorithm algorithmRS = Algorithm.RSA256(publicKey, privateKey);
+//        System.out.println(mostCompetitive(new int[]{71,18,52,29,55,73,24,42,66,8,80,2}, 3));
+        System.out.println(pourWater2(new int[]{1,2,3,4,3,2,1,2,3,4,3,2,1}, 5 ,5));//[1,2,3,4,3,4,3,3,3,4,3,2,1]
     }
 }
