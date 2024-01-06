8e36005bec12339e91493fc9a27f0c30c0a1e4aa
Mars <529714831@qq.com>
2024-01-06 06:54:30 +0000

    test hackerrank

--- src/main/java/LinkList.java
+++ src/main/java/LinkList.java
@@ -35,6 +35,20 @@ public class LinkList {
         return l;
     }
 
+    // array list and map usage
+    public static List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
+    // Write your code here
+        List<Integer> ans = new ArrayList<>();
+        Map<String, Integer> strmap = new HashMap<>();
+        for(String s : stringList){
+            strmap.put(s, strmap.getOrDefault(s, 0) + 1);
+        }  
+        for (String s : queries) {
+            ans.add(strmap.getOrDefault(s, 0));
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(1, new Node(2, new Node(3, null)));
         Node ans = reverseList(head);
