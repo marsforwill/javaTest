3085f6962cc3bb7b49515efa0f6f8522f5d3eb16
Mars <529714831@qq.com>
2024-03-27 02:45:08 +0000

    tree level iterate

--- src/main/java/ArrayListFunc.java
+++ src/main/java/ArrayListFunc.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class ArrayListFunc {
     // new ArrayList<>() / .add / .get
 
+
+  public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+ 
+  public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
     public static List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
         // Write your code here
         List<Integer> ans = new ArrayList<>();
@@ -14,6 +28,39 @@ public class ArrayListFunc {
             ans.add(strmap.getOrDefault(s, 0));
         }
         return ans;
+    }
+
+    // 树的层次遍历 无语法手写真难
+    public static ListNode[] listOfDepth(TreeNode tree) {
+        List<TreeNode> temp = new ArrayList<>();
+        List<ListNode> ans = new ArrayList<>();
+        temp.add(tree);
+        while(temp.size() > 0){
+            // temp - > iter
+            List<TreeNode> iter = new ArrayList<>();
+            ListNode listNode = new ListNode(-1);
+            ListNode head = listNode;
+            for(TreeNode inode : temp){
+                if(inode.left != null){
+                    iter.add(inode.left);
+                }
+                if (inode.right != null){
+                    iter.add(inode.right);
+                }
+                listNode.next = new ListNode(inode.val);
+                listNode = listNode.next;
+            }
+
+            // iter -> ans,temp
+            ans.add(head.next);
+            temp = iter;
+        }
+
+        ListNode[] output = new ListNode[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            output[i] = ans.get(i);
+        }
+        return output;
     }
 
     public static void main(String[] args) {
