4fb9c130cc8027f28b59e81bbe9c6d4812ac9b47
shiminliu <shiminliu@microsoft.com>
2024-05-22 10:09:50 +0800

    finally sorted set fix

--- src/main/java/CodeTest/Content.java
+++ src/main/java/CodeTest/Content.java
@@ -1,6 +1,6 @@
 package CodeTest;
 
-public class Content {
+public class Content implements Comparable<Content> {
 
     public Content(int id, int pop){
         contentId = id;
@@ -13,8 +13,25 @@ public class Content {
 
     @Override
     public boolean equals(Object obj){
+        if (this == obj) return true;
         Content content = (Content)obj;
         return this.contentId == content.contentId;
+    }
+
+    @Override
+	public int hashCode() {
+		return this.hashCode();
+	}
+
+    @Override
+    public int compareTo(Content o) {
+        if(this.contentId == o.contentId){
+            return 0;
+        }
+        if(this.popularity == o.popularity){
+            return Integer.compare(o.contentId, this.contentId);
+        }
+        return Integer.compare(o.popularity, this.popularity);
     }
 
 }
